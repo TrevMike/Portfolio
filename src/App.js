77157e25bcde569bbbd80f16d312edfa8de520bf
TrevMike <preince560m@gmail.com>
2019-06-18 09:41:35 -0500

    Setting everything up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './header/Header'
+// import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React for my portfolio
-        </a>
-      </header>
+    <div>
+      <Header/>
     </div>
   );
 }
---
+++ src/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/header.css'
+
+
+function Header () {
+    return(
+        <div className="typeWriter">
+            Hello My Name is Michael Trevino. 
+            I'm a full stack devolper
+        </div>
+    )
+}
+
+export default Header

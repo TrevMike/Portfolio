9fd1b34d622583e4d2575c333558258165f6a07b
TrevMike <preince560m@gmail.com>
2019-06-23 08:04:39 -0500

    just updating

--- src/aboutMe/AboutMe.js
+++ src/aboutMe/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/index.css'
 import GameRoom from '../assets/GameRoom.gif'
+// import Michael from '../assets/selfie/Michael.jpg'
 // import ScrollableAnchor from 'react-scrollable-anchor';
 
 function AboutMe(){
@@ -12,12 +13,30 @@ function AboutMe(){
             src={GameRoom}
             alt="glowing"
             /> 
+
+            {/* <img
+            className="selfie"
+            src={Michael}
+            alt="myself"
+            /> */}
                 <div className="myLife">
-                    <h1>About Me</h1>
-                    I grew in the bottom peak of Texas in a town called Mcallen.
+                    <h1>Michael-Trevino</h1>
+                    I have found a passion on working with UI animations, effects, and creating<br/>
+                    great dynamic user experiences and always willing to learn to get better.<br/>
+                    <h1>Who am I?</h1>
+                    I grew up in the bottom point of Texas in a town called La Joya. I'm a huge nerd/geek<br/>
+                    I play Magic the gathering and League of Legends on my spare time. I would love to work<br/>
+                    at a StartUp since I have dreams myself and making someone's dream come true would be great!
                 </div>
+
+
         </div>
     )
 }
 
 export default AboutMe
+
+//  1. What are you currently doing (in regard to your career)
+//  and how did you get there? How does your background make you unique?
+// 
+// 2. In terms of the work you do, what aspects are you most passionate about and why?
